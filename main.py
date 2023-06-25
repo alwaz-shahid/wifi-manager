d3e122bab4e6f679838437ab7ee70fa260fe54ca
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-06-25 18:18:22 +0500

    number choice and power control

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main():
         print("1. List USB devices")
         print("2. List network devices")
         print("3. Change wireless adapter mode")
-        print("4. Exit")
+        print("4. Disable wireless adapter")
+        print("5. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -18,6 +19,8 @@ def main():
         elif choice == "3":
             change_wireless_mode()
         elif choice == "4":
+            disable_wireless_adapter()
+        elif choice == "5":
             break
         else:
             print("Invalid choice")
@@ -28,12 +31,24 @@ def list_usb_devices():
 def list_network_devices():
     subprocess.run(["iwconfig"])
     #    subprocess.run(["ip", "link"])
+
 def change_wireless_mode():
     device = input("Enter wireless device name: ")
-    mode = input("Enter mode (managed or monitor): ")
+    mode_choice = input("Enter mode (1 for managed, 2 for monitor): ")
+    if mode_choice == "1":
+        mode = "managed"
+    elif mode_choice == "2":
+        mode = "monitor"
+    else:
+        print("Invalid mode choice")
+        return
     subprocess.run(["sudo", "ip", "link", "set", device, "down"])
     subprocess.run(["sudo", "iwconfig", device, "mode", mode])
     subprocess.run(["sudo", "ip", "link", "set", device, "up"])
+
+def disable_wireless_adapter():
+    device = input("Enter wireless device name: ")
+    subprocess.run(["sudo", "ip", "link", "set", device, "down"])
 
 if __name__ == "__main__":
     main()
